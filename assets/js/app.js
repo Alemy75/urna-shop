e8841116428c6a89948fffe269497c2b0387d336
Alemy75 <alemax75962@gmail.com>
2022-10-31 17:51:51 +0300

    добавление страницы категорий

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,12 +13,4 @@ $(document).ready(function () {
     });
 });
 
-$("a[href^='#']").on("click", function () {
-    let href = $(this).attr("href");
 
-    $("html, body").animate({
-        scrollTop: $(href).offset().top
-    });
-
-    return false;
-});
